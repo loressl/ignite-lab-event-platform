0065b6f464b6283132b7f68a6e7373467d0c09d2
loressl <loryssl@hotmail.com>
2022-06-25 22:14:02 -0300

    fix: Atualiza imagem e centralização de video

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,7 +2,7 @@ import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/genarated";
-
+import img from '../../src/assets/code-mockup.png'
 
 export function Subscribe(){
     const navigate = useNavigate();
@@ -72,7 +72,7 @@ export function Subscribe(){
                 </div>
 
             </div>
-            <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+            <img src={img} className="mt-10" alt="" />
         </div>
     )
 }
